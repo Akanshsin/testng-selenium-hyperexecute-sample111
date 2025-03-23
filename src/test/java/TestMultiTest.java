ddfa1d1eccf82c52bbedc2b70329bd0999035013
Akansh Singhal <akanshs@lambdatest.com>
2025-03-23 20:57:32 +0530

    Update TestMultiTest.java

--- src/test/java/TestMultiTest.java
+++ src/test/java/TestMultiTest.java
@@ -34,7 +34,7 @@ public class TestMultiTest
         capabilities.setCapability("platform", System.getenv("HYPEREXECUTE_PLATFORM"));
         capabilities.setCapability("browserName", browser);
         capabilities.setCapability("version", version);
-        capabilities.setCapability("selenium_version", "4.23.0");
+        capabilities.setCapability("selenium_version", "4.27.0");
 
         capabilities.setCapability("tunnel",false);
         capabilities.setCapability("network",true);
